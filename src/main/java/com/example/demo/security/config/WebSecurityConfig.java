f3663858a2e82f21a82e55dac8d306629267878e
PMushy <44428572+PMushy@users.noreply.github.com>
2021-08-02 18:40:20 +0200

    test loging

--- src/main/java/com/example/demo/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/config/WebSecurityConfig.java
@@ -19,17 +19,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final AppUserService appUserService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    /**With this code, you do not have to log in**/
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/api/v*/registration/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated().and()
-                .formLogin();
+        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
     }
+
+    /**With this code, you do have to log in**/
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/api/v*/registration/**")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated().and()
+//                .formLogin();
+//    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
